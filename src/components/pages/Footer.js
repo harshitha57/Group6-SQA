0dcb9f011d21dd8f19b642d35557b83259f38467
harshitha <harshithachigurupati123@gmail.com>
2024-04-15 14:08:08 -0400

    Initial commit

---
+++ src/components/pages/Footer.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+export function Footer() {
+    return (
+        <div>
+
+            <div className="text-center">
+                <p>Developed by mybank.com</p>
+                <p>All rights reserved by <b><a href="#">mybank.com</a> </b></p>
+            </div>
+        </div>
+    );
+};
